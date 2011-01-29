0a469050257702f4f8e4ffb922385780d2403a41
Visnu Pitiyanuvath <visnupx@gmail.com>
2011-01-29 03:24:47 -0800

    use the current directory for output files

--- generate-combined.sh
+++ generate-combined.sh
@@ -7,13 +7,7 @@ dygraph-canvas.js \
 dygraph.js \
 rgbcolor/rgbcolor.js \
 strftime/strftime-min.js \
-> /tmp/dygraph.js
-
-java -jar yuicompressor-2.4.2.jar /tmp/dygraph.js \
-> /tmp/dygraph-packed.js
-
-# TODO(danvk): ensure the dygraphs copyright, etc. gets into the packed js.
-
-cat \
-/tmp/dygraph-packed.js \
 > dygraph-combined.js
+
+java -jar yuicompressor-2.4.2.jar dygraph-combined.js \
+> dygraph-packed.js
